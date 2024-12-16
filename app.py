a7efb24f4d7c9ffdf00f446eaa5c875e951b7a00
reallywally <goldenale5500@gmail.com>
2024-12-17 00:40:52 +0900

    SQLAlchemy로 CRUD 테스트

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from models import db, Post
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
@@ -13,42 +14,79 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+
 # 게시판 목록
 @app.route('/')
 def index():
     posts = Post.query.all()
-    return render_template('index.html', posts=posts)
+    posts_json = [
+        {"id": post.column_1, "title": post.column_2, "content": post.column_3}
+        for post in posts
+    ]
+    return jsonify(posts_json)
+
 
 # 게시글 생성
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        new_post = Post(title=title, content=content)
+        title = request.json.get('title')
+        content = request.json.get('content')
+        new_post = Post(column_2=title, column_3=content)
         db.session.add(new_post)
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('create.html')
+
+    return jsonify({})
+
 
 # 게시글 수정
-@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+@app.route('/update/<int:post_id>', methods=['GET', 'PATCH'])
 def update(post_id):
     post = Post.query.get_or_404(post_id)
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.content = request.form['content']
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('update.html', post=post)
 
-# 게시글 삭제
-@app.route('/delete/<int:post_id>', methods=['POST'])
-def delete(post_id):
-    post = Post.query.get_or_404(post_id)
-    db.session.delete(post)
+    post.column_2 = request.json.get('title')
+    post.column_3 = request.json.get('content')
     db.session.commit()
-    return redirect(url_for('index'))
+
+    return jsonify({})
+
+
+@app.route('/raw-query', methods=['GET'])
+def raw_query():
+    query = """
+        SELECT *
+        FROM public.post 
+    """
+
+    result = db.session.execute(text(query), )
+    all = result.mappings().all()
+
+    r = [{"title": a.get("column_1"), "content": a.get("column_2")}
+         for a in all]
+
+    return jsonify(r)
+
+
+@app.route('/raw-query-params', methods=['GET'])
+def raw_query_params():
+    query = """
+        SELECT *
+        FROM public.post 
+        WHERE column_1 = :post_id
+    """
+
+    params = {
+        "post_id": 2
+    }
+
+    result = db.session.execute(text(query), params)
+    all = result.mappings().all()
+
+    r = [{"title": a.get("column_1"), "content": a.get("column_2")}
+         for a in all]
+
+    return jsonify(r)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,11 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Post(db.Model):
-    __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+    __tablename__ = 'post'
+    column_1 = db.Column(db.Integer, primary_key=True)
+    column_2 = db.Column(db.String(100), nullable=False)
+    column_3 = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+
+class CompositeKeyTable(db.Model):
+    __tablename__ = 'composite_key_table'
+    column_1 = db.Column(db.Integer, primary_key=True)
+    column_2 = db.Column(db.String(100), nullable=False)
+    column_3 = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f'<Post {self.title}>'
